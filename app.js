ac58f6ec3099e046be4333d1ab25e330958d3a16
chesedi <a02t02@gmail.com>
2016-12-26 17:38:26 +0900

    basic board CRUD updated

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 // import modules
 var express  = require('express');
-var path = require('path');
 var app = express();
+var path = require('path');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
 // connect database
 mongoose.connect(process.env.MONGO_DB);
@@ -15,14 +17,74 @@ db.on("error", function(err){
 });
 
 // model setting
+var postSchema = mongoose.Schema({
+  title : {type : String, required:true},
+  body : {type:String, required:true},
+  createdAt : {type:Date, default:Date.now},
+  updatedAt : Date
+});
+var Post = mongoose.model('post', postSchema);
 
 // view setting
 app.set("view engine", 'ejs');
 
 // set middlewares
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 
 // set routes
+app.get('/posts', function(req, res){
+  Post.find({}).sort('-createdAt').exec(function(err, posts){
+    if(err) return res.json({success: false, message: err});
+    res.render("posts/index", {data:posts});
+  });
+  // Post.find({}, function(err, posts){
+  //   if(err) return res.json({success: false, message: err});
+  //   res.json({success:true, data: posts});
+  // });
+}); // index
+app.get('/posts/new', function(req, res){
+  res.render("posts/new");
+}); // new
+app.post('/posts', function(req, res){
+  console.log(req.body);
+  Post.create(req.body.post, function(err, post){
+    if(err) return res.json({success:false, message:err});
+    res.redirect('/posts');
+    // res.json({success:true, data:post});
+  });
+}); // create
+app.get('/posts/:id', function(req, res){
+  Post.findById(req.params.id, function(err, post){
+    if(err) return res.json({success:false, message:err});
+    // res.json({success:true, data:post});
+    res.render("posts/show", {data:post});
+  });
+}); // show
+app.get('/posts/:id/edit', function(req, res){
+  Post.findById(req.params.id, function(err, post){
+    if(err) return res.json({success:false, message:err});
+    res.render("posts/edit", {data:post});
+  });
+}); // edit
+app.put('/posts/:id', function(req, res){
+  req.body.post.updatedAt = Date.now();
+  console.log('edit');
+  Post.findByIdAndUpdate(req.params.id, req.body.post, function(err, post){
+    if(err) return res.json({success:false, message:err});
+    // res.json({success:true, message:post._id+" updated"});
+    res.redirect('/posts/'+req.params.id);
+  });
+}); // update
+app.delete('/posts/:id', function(req, res){
+  Post.findByIdAndRemove(req.params.id, function(err, post){
+    if(err) return res.json({success:false, message:err});
+    res.redirect('/posts');
+    // res.json({success:true, message:post._id+" deleted"});
+  });
+}); //destroy
 
 // start server
 app.listen(3000, function(){
